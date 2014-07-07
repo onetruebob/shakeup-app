692b02dfadb4a1afc5d53ca9cb759ab90c15620d
Bob Owen <bob@bobowen.org>
2014-07-07 16:26:09 -0700

    On application start, find all the quakes currently in the database and console log the magnitude and location.

---
+++ client/quakedata.js
@@ -0,0 +1,24 @@
+var earthquakeRef = new Firebase("https://publicdata-earthquakes.firebaseio.com/by_continent/");
+var continents = ["europe", "asia", "africa", "north_america", "south_america", "antartica", "oceanic"]; //database is organized by continents
+
+// earthquakeRef.on("child_added", showEarthquake);
+
+function showEarthquake(snapshot) {
+  var earthquake = snapshot.val();
+  console.log("Mag " + earthquake.mag + " at " + earthquake.place);
+}
+
+function start() {
+    for (var con=0; con<continents.length; con++) {
+        var continent = continents[con];
+        for (var mag=0; mag<10; mag++) {
+            earthquakeRef.child(continent)
+                .child(mag.toString())
+                .endAt()
+                .limit(3) // only get the last three quakes of each magnitutde
+                .on('child_added', showEarthquake);
+        }
+    }
+}
+
+start();
