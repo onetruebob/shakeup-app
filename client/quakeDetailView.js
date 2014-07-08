af756523debe2df5a6441e02a312ef384bede47b
Bob Owen <bob@bobowen.org>
2014-07-08 15:28:06 -0700

    Added background and animation when showing information about earthquakes

--- client/quakeDetailView.js
+++ client/quakeDetailView.js
@@ -1,5 +1,6 @@
 (function(){
   var $quakeinfo = $('#quakeinfo');
+  $quakeinfo.hide();
 
   var template = '<h2>Quake: <span id="quakeDetailName"></span></h2> \
                   <dl> \
@@ -16,12 +17,17 @@
   window.quakeDetailView = {};
 
   window.quakeDetailView.showQuake = function(quake) {
+    $quakeinfo.show();
     $quakeinfo.empty();
     $quakeinfo.append(template);
+    $quakeinfo.addClass('animated shake');
     $('#quakeDetailName').text(quake.code);
     $('#quakeDetailMagnitude').text(quake.mag);
     $('#quakeDetailLocation').text(quake.place);
     $('#quakeDetailTime').text((new Date(quake.time)).toString());
     $('#quakeDetailUSGS').append('<a href="' + quake.url + '" target="_blank"> US Geological Survey Data </a>');
+    $quakeinfo.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass('animated shake');
+    });
   }
 })()
