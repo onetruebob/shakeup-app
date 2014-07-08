365f2f83697a9ce3ddc2121fc7e1548ce9344361
Bob Owen <bob@bobowen.org>
2014-07-07 19:13:58 -0700

    Convered quakedata into an IIFE module.

--- client/quakedata.js
+++ client/quakedata.js
@@ -1,24 +1,25 @@
-var earthquakeRef = new Firebase("https://publicdata-earthquakes.firebaseio.com/by_continent/");
-var continents = ["europe", "asia", "africa", "north_america", "south_america", "antartica", "oceanic"]; //database is organized by continents
+(function(){
+  var earthquakeRef = new Firebase("https://publicdata-earthquakes.firebaseio.com/by_continent/");
+  var continents = ["europe", "asia", "africa", "north_america", "south_america", "antartica", "oceanic"]; //database is organized by continents
 
-// earthquakeRef.on("child_added", showEarthquake);
+  function showEarthquake(snapshot) {
+    var earthquake = snapshot.val();
+    globe.addQuake(earthquake);
+  }
 
-function showEarthquake(snapshot) {
-  var earthquake = snapshot.val();
-  globe.addQuake(earthquake);
-}
+  function start() {
+      for (var con=0; con<continents.length; con++) {
+          var continent = continents[con];
+          for (var mag=0; mag<10; mag++) {
+              earthquakeRef.child(continent)
+                  .child(mag.toString())
+                  .endAt()
+                  .limit(3) // only get the last three quakes of each magnitutde
+                  .on('child_added', showEarthquake);
+          }
+      }
+  }
 
-function start() {
-    for (var con=0; con<continents.length; con++) {
-        var continent = continents[con];
-        for (var mag=0; mag<10; mag++) {
-            earthquakeRef.child(continent)
-                .child(mag.toString())
-                .endAt()
-                .limit(3) // only get the last three quakes of each magnitutde
-                .on('child_added', showEarthquake);
-        }
-    }
-}
+  start();
 
-start();
+})();
