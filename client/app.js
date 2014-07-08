9b7ed47e6c36afc634ccda0f50a54147849fd9c1
Bob Owen <bob@bobowen.org>
2014-07-08 08:36:22 -0700

    Moved main app controller to a new file outside the index.

---
+++ client/app.js
@@ -0,0 +1,16 @@
+var $quakeinfo = $('#quakeinfo');
+
+$( "#slider" ).slider({max: window.quakedata.maxMagnitude, 
+                       min: window.quakedata.minMagnitude, 
+                       range: true, 
+                       values: [window.quakedata.minMagnitude, window.quakedata.maxMagnitude]
+                     })
+.on("slide", function(e, ui){
+  $('#minmag').empty().text(ui.values[0]);
+  $('#maxmag').empty().text(ui.values[1]);
+  globe.updateMagnitudeRange(ui.values[0], ui.values[1]);
+});
+
+window.globe.quakeClick = function(quakeData) {
+  $quakeinfo.empty().text("Clicked on quake: " + quakeData.mag);
+};
