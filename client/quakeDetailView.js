bd5f73c92f9dd41403d9ff139408dd41ac47ee3e
Bob Owen <bob@bobowen.org>
2014-07-08 10:16:44 -0700

    Created a template and showing more detail about each quake clicked on.

--- client/quakeDetailView.js
+++ client/quakeDetailView.js
@@ -1,10 +1,24 @@
 (function(){
   var $quakeinfo = $('#quakeinfo');
 
+  var template = '<h2>Quake: <span id="quakeDetailName"></span></h2> \
+                  <dl> \
+                  <dt>Magnitude:</dt> \
+                  <dd id="quakeDetailMagnitude"></dd> \
+                  <dt>Location:</dt> \
+                  <dd id="quakeDetailLocation"></dd> \
+                  <dt>Date / Time:</dt> \
+                  <dd id="quakeDetailTime"></dd> \
+                  </dl>';
+
   window.quakeDetailView = {};
 
   window.quakeDetailView.showQuake = function(quake) {
     $quakeinfo.empty();
-    $quakeinfo.append('<p>Quake happened in: ' + quake.place + '</p>');
+    $quakeinfo.append(template);
+    $('#quakeDetailName').text(quake.code);
+    $('#quakeDetailMagnitude').text(quake.mag);
+    $('#quakeDetailLocation').text(quake.place);
+    $('#quakeDetailTime').text((new Date(quake.time)).toString());
   }
 })()
